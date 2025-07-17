ac917adf81b6430aea12b6aad381568465c7bef9
Casey Francisco <caseyfrancisco13@gmail.com>
2025-07-17 19:46:17 +0800

    added supabase, establishing sections

--- src/pages/Navi.jsx
+++ src/pages/Navi.jsx
@@ -4,9 +4,9 @@ export default function Navi() {
   return (
     <div className='navigator-container'>
       <div className='navigator-subcontainer'>
-        <h1>1</h1>
-        <h1>2</h1>
-        <h1>3</h1>
+        <h1>Data Structures</h1>
+        <h1>Advanced Programming</h1>
+        <h1>Data Analysis</h1>
       </div>
     </div>
   )
