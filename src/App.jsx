6a241cf245ff4030b80fc0555e5c9d16bc6d5895
Casey Francisco <caseyfrancisco13@gmail.com>
2025-07-18 15:42:57 +0800

    tweaking hero page and nav css

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ import Home from './pages/Home'
  */
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,16 @@ export default function Home() {
   return (
     <div className='hero-container'>
       <h2>Hero Page Contents</h2>
+      <div className='hero-subcontainer'>
+        <div className='hero-statement'>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque, accusamus, quisquam quibusdam modi suscipit dignissimos perspiciatis nostrum itaque, optio enim quis nisi alias inventore saepe! Obcaecati autem dolorum doloribus recusandae!
+          </p>
+        </div>
+        <div className='hero-logo'>
+          <h1> logo here </h1>
+        </div>
+      </div>
     </div>
   )
 }
